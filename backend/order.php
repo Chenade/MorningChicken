dc825822bb3febc65fd4fe5ca27a799ea6951f43
YANGCHI KUO <chenade0312@gmail.com>
2023-11-13 00:03:02 +0100

    add cart system

---
+++ backend/order.php
@@ -0,0 +1,23 @@
+<?php
+
+// add a data to order table
+include_once("connect.php");
+
+$result = array("success" => true);
+$_POST['cart'] = json_encode($_POST['cart']);
+
+$sql = "INSERT INTO `orders`(`name`, `phone`, `restaurant`, `cart`, `pickup`) VALUES ('" . $_POST['name'] . "', '" . $_POST['phone'] . "', '" . $_POST['restaurant_name'] . "', '" . $_POST['cart'] . "', '" . $_POST['time'] . "')";
+$result = $conn->query($sql);
+if ($result === false) {
+    // echo "Error executing the query: " . $conn->error;
+    $result = array("success" => false);
+    array_push($result, array('error' => $conn->error));
+} else {
+    // echo "Order added successfully";
+}
+
+$conn->close();
+
+echo json_encode($result);
+
+?>
